fd26fa02f30d04d2a835d5a6e056bac7deb4b6f8
Woliver4 <woliver4@gmail.com>
2023-03-29 15:20:36 +0200

    first commit

---
+++ Challenges/Challenge 2/scripts.js
@@ -0,0 +1,7 @@
+// scripts.js
+
+import {role as CEO} from './nwabisa.js'
+import {role as Intern} from './johannes.js'
+import {role as HOM} from './alex.js'
+
+console.log('Roles:', CEO, Intern, HOM)
